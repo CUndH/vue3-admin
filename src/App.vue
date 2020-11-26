22ada91342fa67a012b90be396816767a30bb0b3
陈元皓 <joeycairo@xingren.com>
2020-11-26 10:18:09 +0800

    项目结构初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-config-provider :locale="locale">
+    <router-view />
+  </a-config-provider>
+</template>
+
+<script lang="ts">
+/**
+ * @author cyh
+ */
+
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {
+      locale: zhCN,
+    };
+  },
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import App from './App.vue';
+import { router } from './router/index';
+import store from './store';
+import '@/styles/main.scss';
+
+createApp(App).use(store).use(Antd).use(router)
+  .mount('#app');
